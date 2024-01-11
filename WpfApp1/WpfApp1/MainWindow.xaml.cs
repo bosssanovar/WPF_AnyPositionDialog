2da209129cdcefc30e3811516ccdf22ebc18096c
kei ito <ito.kei@jvckenwood.com>
2024-01-11 10:04:25 +0900

    ボタンクリック時表示対応。

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -20,5 +20,37 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Get absolute location on screen of upper left corner of the UserControl
+            Point locationFromScreen = button1.PointToScreen(new Point(0, 0));
+            // Transform screen point to WPF device independent point
+            PresentationSource source = PresentationSource.FromVisual(this);
+            Point targetPoints = source.CompositionTarget.TransformFromDevice.Transform(locationFromScreen);
+            // Get Focus
+            Point focus = new Point();
+            focus.X = targetPoints.X;
+            focus.Y = targetPoints.Y;
+            // Set coordinates
+            Window window = new SubWindow
+            {
+                Owner = this,
+                WindowStartupLocation = WindowStartupLocation.Manual,
+            };
+            window.Top = focus.Y;
+            window.Left = focus.X;
+            window.ShowDialog();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window window = new SubWindow
+            {
+                Owner = this,
+                WindowStartupLocation = WindowStartupLocation.CenterOwner
+            };
+            window.ShowDialog();
+        }
     }
 }
