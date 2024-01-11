2a02361e590c51d5c630777b12b0eb9cfd96a024
kei ito <ito.kei@jvckenwood.com>
2024-01-11 10:19:01 +0900

    RF:任意位置表示処理を汎用クラスに移動

---
+++ WpfApp1/WpfApp1/DialogHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace WpfApp1
+{
+    internal class DialogHelper
+    {
+        public static void ShowDialogOnTarget(Window window, Button target)
+        {
+            // Get absolute location on screen of upper left corner of the UserControl
+            Point locationFromScreen = target.PointToScreen(new Point(0, 0));
+            // Transform screen point to WPF device independent point
+            PresentationSource source = PresentationSource.FromVisual(window);
+            Point targetPoints = source.CompositionTarget.TransformFromDevice.Transform(locationFromScreen);
+            // Get Focus
+            Point focus = new Point();
+            focus.X = targetPoints.X;
+            focus.Y = targetPoints.Y;
+            // Set coordinates
+            Window subWindow = new SubWindow
+            {
+                Owner = window,
+                WindowStartupLocation = WindowStartupLocation.Manual,
+            };
+            subWindow.Top = focus.Y;
+            subWindow.Left = focus.X;
+            subWindow.ShowDialog();
+        }
+    }
+}
--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -23,24 +23,8 @@ namespace WpfApp1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            // Get absolute location on screen of upper left corner of the UserControl
-            Point locationFromScreen = button1.PointToScreen(new Point(0, 0));
-            // Transform screen point to WPF device independent point
-            PresentationSource source = PresentationSource.FromVisual(this);
-            Point targetPoints = source.CompositionTarget.TransformFromDevice.Transform(locationFromScreen);
-            // Get Focus
-            Point focus = new Point();
-            focus.X = targetPoints.X;
-            focus.Y = targetPoints.Y;
-            // Set coordinates
-            Window window = new SubWindow
-            {
-                Owner = this,
-                WindowStartupLocation = WindowStartupLocation.Manual,
-            };
-            window.Top = focus.Y;
-            window.Left = focus.X;
-            window.ShowDialog();
+            var target = button1;
+            DialogHelper.ShowDialogOnTarget(this, target);
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
