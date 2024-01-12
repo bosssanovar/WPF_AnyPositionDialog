d74efcb03b3c4b685196637eefd7f072225d5fa7
kei ito <ito.kei@jvckenwood.com>
2024-01-12 13:33:10 +0900

    RF:不要コード削除

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -18,11 +18,9 @@ namespace WpfApp1
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
         public ObservableCollection<Detail> DetailList { get; } = new ObservableCollection<Detail>();
-
-        public event PropertyChangedEventHandler? PropertyChanged;
 
         public MainWindow()
         {
